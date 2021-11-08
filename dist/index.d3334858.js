287f4fd344ff3dd8c51d0a0c77ffdc515b751824
gabbybamps <gabbybamps@gmail.com>
2021-11-08 10:18:35 +0000

    Added start

---
+++ dist/index.d3334858.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll(`.btn`);
+const imgs = document.querySelectorAll(`.store-item`);
+const filterGallery = function() {
+    buttons.forEach((button)=>{
+        button.addEventListener(`click`, (e)=>{
+            e.preventDefault();
+            const filter = e.target.dataset.filter;
+            imgs.forEach((item)=>{
+                if (filter === `all`) item.style.display = `block`;
+                else if (item.classList.contains(filter)) item.style.display = `block`;
+                else item.style.display = `none`;
+            });
+        });
+    });
+};
+filterGallery();
+
+//# sourceMappingURL=index.d3334858.js.map
